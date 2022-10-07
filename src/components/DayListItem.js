2c92f7367df1029ba26c20815bfff252aca147e6
Jacqline <jacqlinegeng@gmail.com>
2022-10-06 23:13:34 -0400

    added stories for interviewer scheduler

---
+++ src/components/DayListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+function formatSpots(spots) {
+  if (spots === 0) {
+    return "no spots remaining";
+  }
+
+  return `${spots} ${spots === 1 ? 'spot' : 'spots'} remaining`;
+}
+
+export default function DayListItem(props) {
+  const dayClass = classNames('day-list__item', { 'day-list__item--selected': props.selected === true }, { 'day-list__item--full': props.spots === 0})
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected} >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.formatSpots()}</h3>
+    </li>
+  );
+}
