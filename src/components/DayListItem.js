5cfc733a4fa06291e3831fcd7a73066af04c276e
Jacqline <jacqlinegeng@gmail.com>
2022-10-13 20:00:42 -0400

    added cypress tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -14,7 +14,12 @@ export default function DayListItem(props) {
   const dayClass = classNames('day-list__item', { 'day-list__item--selected': props.selected === true }, { 'day-list__item--full': props.spots === 0})
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected} >
+    <li 
+      data-testid="day"
+      className={dayClass} 
+      onClick={() => props.setDay(props.name)} 
+      selected={props.selected} 
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
